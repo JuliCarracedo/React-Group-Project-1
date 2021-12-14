e1ba6b0e4c43e4858f81f80abcc6f9ac01bce0bd
JuliCarracedo <julicarra@hotmail.com>
2021-12-14 17:14:24 -0300

    connect to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import './App.css';
+import Missions from './pages/Missions/Missions';
+import Profile from './pages/Profile';
 
 const App = () => (
   <div>
     <Router>
       <Header />
-      <Routes />
+      <Routes>
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/missions" element={<Profile />} />
+      </Routes>
     </Router>
   </div>
 
