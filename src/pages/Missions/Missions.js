a4f7ac1481f7b1e1d0b331ad4bf853f2db2f5fd7
JuliCarracedo <julicarra@hotmail.com>
2021-12-14 17:03:15 -0300

    Add profile page

---
+++ src/pages/Missions/Missions.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const missionsList = [
+  {
+    missionName: 'Thaicom',
+    missionDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sem erat, euismod at eros luctus, vulputate laoreet arcu. Duis a pharetra neque. Donec auctor nec sapien id pulvinar. Proin egestas, libero in pellentesque lobortis, nulla metus sodales mauris, mattis imperdiet ante ex eu urna. Nullam dictum eget enim vitae sagittis. Vestibulum elementum auctor dui auctor consequat.',
+    activeMember: false,
+  },
+  {
+    missionName: 'Command',
+    missionDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sem erat, euismod at eros luctus, vulputate laoreet arcu. Duis a pharetra neque. Donec auctor nec sapien id pulvinar. Proin egestas, libero in pellentesque lobortis, nulla metus sodales mauris, mattis imperdiet ante ex eu urna. Nullam dictum eget enim vitae sagittis. Vestibulum elementum auctor dui auctor consequat.',
+    activeMember: true,
+  },
+  {
+    missionName: 'Goliath',
+    missionDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sem erat, euismod at eros luctus, vulputate laoreet arcu. Duis a pharetra neque. Donec auctor nec sapien id pulvinar. Proin egestas, libero in pellentesque lobortis, nulla metus sodales mauris, mattis imperdiet ante ex eu urna. Nullam dictum eget enim vitae sagittis. Vestibulum elementum auctor dui auctor consequat.',
+    activeMember: false,
+  },
+];
+
+const Missions = () => (
+  <div > 
+      
+  </div> 
+);
+
+export default Missions;
