c1db575b12bc3787e8072a6eb14ca3da906eb9c0
JUNIOR FONDEM <fondemjunior@yahoo.com>
2021-12-15 18:46:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
-import store from './redux/configureStore';
 
 ReactDOM.render(
   <StrictMode>
