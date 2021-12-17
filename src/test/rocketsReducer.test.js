1044562e65de17e798f155ee6e7b10a8944020fe
codepanther <pezechigozirim@gmail.com>
2021-12-17 10:55:18 +0100

    Start writing unit tests for the rocketsReducer

---
+++ src/test/rocketsReducer.test.js
@@ -0,0 +1,12 @@
+import rocketsReducer from "../redux/rockets/rockets";
+
+describe('rocketsReducer', () => {
+  const rockets = [
+    {
+      id: 1,
+      name: 'Some rocket',
+      
+    }
+
+  ]
+})
