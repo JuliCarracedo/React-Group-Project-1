32838350d9d8e7be8a0448c8ece2076ac7fb2140
codepanther <pezechigozirim@gmail.com>
2021-12-17 07:08:37 +0100

    Implement active navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../Assets/logo.png';
 
-const Header = () => (
-  <div className="headerContainer">
-    <div>
-      <img src={logo} alt="SpaceX logo" style={{ width: '50px', height: '50px' }} className="logo" />
-      <span className="headerNav"><h1>Space Travelers&apos; Hub</h1></span>
+const Header = () => {
+  const [activeLink, setActiveLink] = useState('/');
+  return (
+    <div className="headerContainer">
+      <div>
+        <img
+          src={logo}
+          alt="SpaceX logo"
+          style={{ width: '50px', height: '50px' }}
+          className="logo"
+        />
+        <span className="headerNav">
+          <h1>Space Travelers&apos; Hub</h1>
+        </span>
+      </div>
+      <div>
+        <ul>
+          <NavLink
+            to="/"
+            onClick={() => setActiveLink('/')}
+            style={{ textDecoration: activeLink === '/' ? 'underline' : '' }}
+          >
+            Rockets
+          </NavLink>
+          <NavLink
+            to="/missions"
+            onClick={() => setActiveLink('mission')}
+            style={{
+              textDecoration: activeLink === 'mission' ? 'underline' : '',
+            }}
+          >
+            Missions
+          </NavLink>
+          <NavLink
+            to="/dragons"
+            onClick={() => setActiveLink('dragons')}
+            style={{
+              textDecoration: activeLink === 'dragons' ? 'underline' : '',
+            }}
+          >
+            Dragons
+          </NavLink>
+          <NavLink
+            to="/profile"
+            onClick={() => setActiveLink('profile')}
+            style={{
+              textDecoration: activeLink === 'profile' ? 'underline' : '',
+            }}
+          >
+            My Profile
+          </NavLink>
+        </ul>
+      </div>
     </div>
-    <div>
-      <ul>
-        <NavLink exact to="/" activeClassName="active-link">Rockets</NavLink>
-        <NavLink to="/missions" activeClassName="active-link">Missions</NavLink>
-        <NavLink to="/dragons" activeClassName="active-link">Dragons</NavLink>
-        <NavLink to="/profile" activeClassName="active-link">My Profile</NavLink>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
